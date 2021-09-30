eeb8c0f781f967f15c87f07c4952b5590b05ec82
uk2459644 <uk2459644@gmail.com>
2021-09-30 06:06:41 +0530

    uk-editorjs-alert

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require("webpack");
+const path = require('path');
+
+module.exports = (env, argv) => {
+    const { mode } = argv;
+  
+    return {
+      entry:'./src/index',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+                // query: {
+                //   presets: ['@babel/preset-env'],
+                // },
+                options: {
+                    presets: ['@babel/preset-env']
+                  },
+
+              },
+            ],
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.svg$/,
+            // loader: 'svg-inline-loader?removeSVGTagAttrs=false',
+            type: 'asset/resource',
+          },
+        ],
+      },
+      output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: mode === 'production' ? 'bundle.js' : `${mode}.bundle.js`,
+        library: 'Alert',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+      },
+    };
+  };
+  
